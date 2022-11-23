6786fac189766177da0d6b0093bd7b1613d7cd09
Hoss Agina <hoss.agina@telus.com>
2022-11-23 15:58:35 -0700

    creating new file input.js and moved 2 functions from client.js to input.js

--- client.js
+++ client.js
@@ -24,20 +24,4 @@ const connect = function () {
   return conn;
 };
 
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.on("data", handleUserInput);
-  stdin.resume();
-  return stdin;
-};
-
-module.exports = {connect, setupInput, handleUserInput};
+module.exports = {connect};
