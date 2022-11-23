6a318f98ac0664b613eaecfd20b2a2c0ec4e2c0a
Hoss Agina <hoss.agina@telus.com>
2022-11-23 14:22:14 -0700

    added move up,down,right,left commands and used setTimeout and setinterval to check how snake moves

--- client.js
+++ client.js
@@ -11,6 +11,11 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: HAA");
+    // setTimeout(() => {conn.write("Move: up")}, 1000);
+    // setTimeout(() => {conn.write("Move: left")}, 2000);
+    // setTimeout(() => {conn.write("Move: down")}, 3000);
+    // setTimeout(() => {conn.write("Move: left")}, 4000);
+    // setInterval(() => {conn.write("Move: right")}, 50);
   })
   conn.on("data", (data) => {
     console.log("Server says: ", data);
