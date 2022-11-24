e8f517c6bd70588f2b8033dd01051ac6f80465fc
Hoss Agina <hoss.agina@telus.com>
2022-11-23 18:10:19 -0700

    adding finalized adjusted files

--- client.js
+++ client.js
@@ -5,17 +5,11 @@ const connect = function () {
     host: "localhost",
     port: 50541,
   });
-
-  // interpret incoming data as text
+  
   conn.setEncoding("utf8");
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: HAA");
-    // setTimeout(() => {conn.write("Move: up")}, 1000);
-    // setTimeout(() => {conn.write("Move: left")}, 2000);
-    // setTimeout(() => {conn.write("Move: down")}, 3000);
-    // setTimeout(() => {conn.write("Move: left")}, 4000);
-    // setInterval(() => {conn.write("Move: right")}, 50);
   })
   conn.on("data", (data) => {
     console.log("Server says: ", data);
